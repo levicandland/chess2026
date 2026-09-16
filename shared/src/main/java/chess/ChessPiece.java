b78c706ff7be42fc8971b3a9279da7ec53c2e792
Levi <levican@byu.edu>
2026-09-16 17:32:20 -0600

    piece moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -81,6 +81,22 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        switch (getPieceType()){
+            case KING:
+                throw new RuntimeException("Not implemented");
+            case QUEEN:
+                throw new RuntimeException("Not implemented");
+            case BISHOP:
+                throw new RuntimeException("Not implemented");
+            case KNIGHT:
+                return knightMoves(board, myPosition);
+            case ROOK:
+                throw new RuntimeException("Not implemented");
+            case PAWN:
+                throw new RuntimeException("Not implemented");
+            default:
+                throw new RuntimeException("Not implemented");
+        }
+
     }
 }
