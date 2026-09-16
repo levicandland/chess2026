a63f648dc1ca51c370e368ff29f3721885f5b1dd
Levi <levican@byu.edu>
2026-09-15 18:51:48 -0600

    Chess Move complete

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -8,8 +10,22 @@ package chess;
  */
 public class ChessPosition {
     //create row and column
-    private int row;
-    private int column;
+    private final int row;
+    private final int column;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return row == that.row && column == that.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
 
     public ChessPosition(int row, int column) {
         this.row = row;
